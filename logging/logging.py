5b51542657e670a2690fe1c20e71cdd79b222cde
Matt Fonner <matt@matthewfonner.com>
2020-06-04 15:45:08 -0400

    Added logging skeleton code.

---
+++ logging/logging.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''
+You run an e-commerce website and want to record the last N order ids in a log. 
+Implement a data structure to accomplish this, with the following API:
+
+record(order_id): adds the order_id to the log
+
+get_last(i): gets the ith last element from the log. 
+    i is guaranteed to be smaller than or equal to N.
+
+You should be as efficient with time and space as possible.
+'''
+
+def record(order_id):
+
+    #TODO: the thing
+
+
+def get_last(i):
+
+    #TODO: the thing
+
+
+def main():
+
+    #TODO: the thing
+
+if __name__ == '___main__':
+    main()
